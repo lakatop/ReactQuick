1e9dde34959f02bbc89d129434fdd8692a7d0f22
Peter Lakatos <lakatopeter8@gmail.com>
2022-04-15 12:53:13 +0200

    added new navigation bar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,29 +1,39 @@
 import { Button } from "@mui/material";
 import { Outlet, Link } from "react-router-dom";
 
-const Layout = () => {
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+
+const ResponsiveAppBar = () => {
+
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/"><Button>Home</Button></Link>
-          </li>
-          <li>
-            <Link to="/second"><Button>Second</Button></Link>
-          </li>
-          <li>
-            <Link to="/increment"><Button>Increment</Button></Link>
-          </li>
-          <li>
-            <Link to="/decrement"><Button>Decrement</Button></Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Link to="/">
+              <Button key="home" sx={{ my: 2, color: 'white', display: 'block' }}> Home </Button>
+            </Link>
+            <Link to="/second">
+              <Button key="second" sx={{ my: 2, color: 'white', display: 'block' }}>Second</Button>
+            </Link>
+            <Link to="/increment">
+              <Button key="increment" sx={{ my: 2, color: 'white', display: 'block' }}>Increment</Button>
+            </Link>
+            <Link to="/decrement">
+              <Button key="decrement" sx={{ my: 2, color: 'white', display: 'block' }}>Decrement</Button>
+            </Link>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    <br></br>
+    <Outlet />
     </>
-  )
+  );
 };
-
-export default Layout;
+export default ResponsiveAppBar;
